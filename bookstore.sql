b0c547b4d7e3428a2aec145e84dae4019e9ec688
Briotang <Tangpeeep@gmail.com>
2025-04-25 12:50:21 +0300

    submission

--- bookstore.sql
+++ bookstore.sql
@@ -1,427 +1,195 @@
-CREATE DATABASE  IF NOT EXISTS `bookstore_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `bookstore_db`;
--- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
---
--- Host: localhost    Database: bookstore_db
--- ------------------------------------------------------
--- Server version	8.0.41
+-- This SQL script creates a database schema for a bookstore system, including tables for books, authors, customers, orders, and more. It also includes user roles and permissions.
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+-- table publisher
+CREATE TABLE publisher (
+    publisher_id INT AUTO_INCREMENT PRIMARY KEY,
+    publisher_name VARCHAR(100),
+    website VARCHAR(100)
+);
+-- table book_language
 
---
--- Table structure for table `address`
---
+CREATE TABLE book_language (
+    language_id INT AUTO_INCREMENT PRIMARY KEY,
+    language_name VARCHAR(255)
+);
+-- table book
 
-DROP TABLE IF EXISTS `address`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `address` (
-  `address_id` int NOT NULL AUTO_INCREMENT,
-  `street_number` varchar(100) DEFAULT NULL,
-  `street_name` varchar(100) DEFAULT NULL,
-  `city` varchar(100) DEFAULT NULL,
-  `country_id` int DEFAULT NULL,
-  PRIMARY KEY (`address_id`),
-  KEY `country_id` (`country_id`),
-  CONSTRAINT `address_ibfk_1` FOREIGN KEY (`country_id`) REFERENCES `country` (`country_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE book (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255),
+    isbn VARCHAR(255),
+    language_id INT,
+    num_pages INT,
+    price INT,
+    publication_date DATE,
+    stock INT,
+    publisher_id INT,
+    FOREIGN KEY (publisher_id)
+        REFERENCES publisher (publisher_id),
+    FOREIGN KEY (language_id)
+        REFERENCES book_language (language_id)
+);
 
---
--- Dumping data for table `address`
---
-  
-LOCK TABLES `address` WRITE;
-/*!40000 ALTER TABLE `address` DISABLE KEYS */;
-/*!40000 ALTER TABLE `address` ENABLE KEYS */;
-UNLOCK TABLES;
+-- table author
+CREATE TABLE author (
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(100)
+);
 
---
--- Table structure for table `address_status`
---
+-- table book_author
+CREATE TABLE book_author (
+    book_id INT,
+    author_id INT,
+    PRIMARY KEY (book_id , author_id),
+    FOREIGN KEY (book_id)
+        REFERENCES book (book_id),
+    FOREIGN KEY (author_id)
+        REFERENCES author (author_id)
+);
 
-DROP TABLE IF EXISTS `address_status`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `address_status` (
-  `status_id` int NOT NULL AUTO_INCREMENT,
-  `address_status` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`status_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+--table customer
+CREATE TABLE customer (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    email VARCHAR(100) UNIQUE,
+    phone INT
+);
 
---
--- Dumping data for table `address_status`
---
+-- table country
+CREATE TABLE country (
+    country_id INT AUTO_INCREMENT PRIMARY KEY,
+    country_name VARCHAR(100)
+);
 
-LOCK TABLES `address_status` WRITE;
-/*!40000 ALTER TABLE `address_status` DISABLE KEYS */;
-/*!40000 ALTER TABLE `address_status` ENABLE KEYS */;
-UNLOCK TABLES;
+-- table address
+CREATE TABLE address (
+    address_id INT AUTO_INCREMENT PRIMARY KEY,
+    street_number VARCHAR(100),
+    street_name VARCHAR(100),
+    city VARCHAR(100),
+    country_id INT,
+    FOREIGN KEY (country_id)
+        REFERENCES country (country_id)
+);
 
---
--- Table structure for table `author`
---
+-- table address_status
+CREATE TABLE address_status (
+    status_id INT AUTO_INCREMENT PRIMARY KEY,
+    address_status VARCHAR(100)
+);
 
-DROP TABLE IF EXISTS `author`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `author` (
-  `author_id` int NOT NULL AUTO_INCREMENT,
-  `author_name` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`author_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- table customer_address
+CREATE TABLE customer_address (
+    customer_id INT,
+    address_id INT,
+    status_id INT,
+    PRIMARY KEY (customer_id , address_id),
+    FOREIGN KEY (customer_id)
+        REFERENCES customer (customer_id),
+    FOREIGN KEY (address_id)
+        REFERENCES address (address_id),
+    FOREIGN KEY (status_id)
+        REFERENCES address_status (status_id)
+);
 
---
--- Dumping data for table `author`
---
+-- table order_status
+CREATE TABLE order_status (
+    status_id INT AUTO_INCREMENT PRIMARY KEY,
+    status_name VARCHAR(255)
+);
 
-LOCK TABLES `author` WRITE;
-/*!40000 ALTER TABLE `author` DISABLE KEYS */;
-/*!40000 ALTER TABLE `author` ENABLE KEYS */;
-UNLOCK TABLES;
+-- table order_history
+CREATE TABLE order_history (
+    history_id INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT,
+    status_id INT,
+    status_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (order_id)
+        REFERENCES cust_order (order_id),
+    FOREIGN KEY (status_id)
+        REFERENCES order_status (status_id)
+);
 
---
--- Table structure for table `book`
---
+-- table cust_order
+CREATE TABLE cust_order (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT,
+    order_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    shipping_method_id INT,
+    status_id INT,
+    FOREIGN KEY (customer_id)
+        REFERENCES customer (customer_id),
+    FOREIGN KEY (shipping_method_id)
+        REFERENCES shipping_method (shipping_method_id),
+    FOREIGN KEY (status_id)
+        REFERENCES order_status (status_id)
+);
 
-DROP TABLE IF EXISTS `book`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `book` (
-  `book_id` int NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) DEFAULT NULL,
-  `isbn` varchar(255) DEFAULT NULL,
-  `language_id` int DEFAULT NULL,
-  `num_pages` int DEFAULT NULL,
-  `price` int DEFAULT NULL,
-  `publication_date` date DEFAULT NULL,
-  `stock` int DEFAULT NULL,
-  `publisher_id` int DEFAULT NULL,
-  PRIMARY KEY (`book_id`),
-  KEY `publisher_id` (`publisher_id`),
-  KEY `language_id` (`language_id`),
-  CONSTRAINT `book_ibfk_1` FOREIGN KEY (`publisher_id`) REFERENCES `publisher` (`publisher_id`),
-  CONSTRAINT `book_ibfk_2` FOREIGN KEY (`language_id`) REFERENCES `book_language` (`language_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- table order_line
+CREATE TABLE order_line (
+    line_id INT,
+    book_id INT,
+    order_id INT,
+    quantity INT,
+    price_each INT,
+    PRIMARY KEY (line_id , book_id),
+    FOREIGN KEY (order_id)
+        REFERENCES cust_order (order_id),
+    FOREIGN KEY (book_id)
+        REFERENCES book (book_id)
+);
 
---
--- Dumping data for table `book`
---
+-- table shipping_method
+CREATE TABLE shipping_method (
+    shipping_method_id INT AUTO_INCREMENT PRIMARY KEY,
+    method_name VARCHAR(100),
+    cost INT
+);
 
-LOCK TABLES `book` WRITE;
-/*!40000 ALTER TABLE `book` DISABLE KEYS */;
-/*!40000 ALTER TABLE `book` ENABLE KEYS */;
-UNLOCK TABLES;
+-- create roles and users
+create role admins, readonly;
+grant all on bookstore_db.* to admins;
+grant select on bookstore_db.* to readonly;
+create user 'admin_user'@'localhost' 
+identified by "123456789";
 
---
--- Table structure for table `book_author`
---
+create user 'readonly_user'@'localhost'
+identified by "123456789";
 
-DROP TABLE IF EXISTS `book_author`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `book_author` (
-  `book_id` int NOT NULL,
-  `author_id` int NOT NULL,
-  PRIMARY KEY (`book_id`,`author_id`),
-  KEY `author_id` (`author_id`),
-  CONSTRAINT `book_author_ibfk_1` FOREIGN KEY (`book_id`) REFERENCES `book` (`book_id`),
-  CONSTRAINT `book_author_ibfk_2` FOREIGN KEY (`author_id`) REFERENCES `author` (`author_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+grant admins to 'admin_user'@'localhost' ;
+grant readonly to 'readonly_user'@'localhost' ;
 
---
--- Dumping data for table `book_author`
---
+-- example queries
+SELECT 
+    c.customer_id,
+    c.first_name,
+    c.last_name,
+    COUNT(ca.address_id) AS address_count
+FROM
+    customer c
+        JOIN
+    customer_address ca ON c.customer_id = ca.customer_id
+GROUP BY c.customer_id
+HAVING address_count > 1;
 
-LOCK TABLES `book_author` WRITE;
-/*!40000 ALTER TABLE `book_author` DISABLE KEYS */;
-/*!40000 ALTER TABLE `book_author` ENABLE KEYS */;
-UNLOCK TABLES;
+SELECT 
+    oh.order_id, os.status_name, oh.status_date
+FROM
+    order_history oh
+        JOIN
+    order_status os ON oh.status_id = os.status_id
+ORDER BY oh.order_id , oh.status_date;
 
---
--- Table structure for table `book_language`
---
-
-DROP TABLE IF EXISTS `book_language`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `book_language` (
-  `language_id` int NOT NULL AUTO_INCREMENT,
-  `language_name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`language_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `book_language`
---
-
-LOCK TABLES `book_language` WRITE;
-/*!40000 ALTER TABLE `book_language` DISABLE KEYS */;
-/*!40000 ALTER TABLE `book_language` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `country`
---
-
-DROP TABLE IF EXISTS `country`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `country` (
-  `country_id` int NOT NULL AUTO_INCREMENT,
-  `country_name` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`country_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `country`
---
-
-LOCK TABLES `country` WRITE;
-/*!40000 ALTER TABLE `country` DISABLE KEYS */;
-/*!40000 ALTER TABLE `country` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `cust_order`
---
-
-DROP TABLE IF EXISTS `cust_order`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `cust_order` (
-  `order_id` int NOT NULL AUTO_INCREMENT,
-  `customer_id` int DEFAULT NULL,
-  `order_date` datetime DEFAULT CURRENT_TIMESTAMP,
-  `shipping_method_id` int DEFAULT NULL,
-  `status_id` int DEFAULT NULL,
-  PRIMARY KEY (`order_id`),
-  KEY `customer_id` (`customer_id`),
-  KEY `shipping_method_id` (`shipping_method_id`),
-  KEY `status_id` (`status_id`),
-  CONSTRAINT `cust_order_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`),
-  CONSTRAINT `cust_order_ibfk_2` FOREIGN KEY (`shipping_method_id`) REFERENCES `shipping_method` (`shipping_method_id`),
-  CONSTRAINT `cust_order_ibfk_3` FOREIGN KEY (`status_id`) REFERENCES `order_status` (`status_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `cust_order`
---
-
-LOCK TABLES `cust_order` WRITE;
-/*!40000 ALTER TABLE `cust_order` DISABLE KEYS */;
-/*!40000 ALTER TABLE `cust_order` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `customer`
---
-
-DROP TABLE IF EXISTS `customer`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `customer` (
-  `customer_id` int NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(100) DEFAULT NULL,
-  `last_name` varchar(100) DEFAULT NULL,
-  `email` varchar(100) DEFAULT NULL,
-  `phone` int DEFAULT NULL,
-  PRIMARY KEY (`customer_id`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `customer`
---
-
-LOCK TABLES `customer` WRITE;
-/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
-/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `customer_address`
---
-
-DROP TABLE IF EXISTS `customer_address`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `customer_address` (
-  `customer_id` int NOT NULL,
-  `address_id` int NOT NULL,
-  `status_id` int DEFAULT NULL,
-  PRIMARY KEY (`customer_id`,`address_id`),
-  KEY `address_id` (`address_id`),
-  KEY `status_id` (`status_id`),
-  CONSTRAINT `customer_address_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`),
-  CONSTRAINT `customer_address_ibfk_2` FOREIGN KEY (`address_id`) REFERENCES `address` (`address_id`),
-  CONSTRAINT `customer_address_ibfk_3` FOREIGN KEY (`status_id`) REFERENCES `address_status` (`status_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `customer_address`
---
-
-LOCK TABLES `customer_address` WRITE;
-/*!40000 ALTER TABLE `customer_address` DISABLE KEYS */;
-/*!40000 ALTER TABLE `customer_address` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `order_history`
---
-
-DROP TABLE IF EXISTS `order_history`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `order_history` (
-  `history_id` int NOT NULL AUTO_INCREMENT,
-  `order_id` int DEFAULT NULL,
-  `status_id` int DEFAULT NULL,
-  `status_date` datetime DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`history_id`),
-  KEY `order_id` (`order_id`),
-  KEY `status_id` (`status_id`),
-  CONSTRAINT `order_history_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `cust_order` (`order_id`),
-  CONSTRAINT `order_history_ibfk_2` FOREIGN KEY (`status_id`) REFERENCES `order_status` (`status_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `order_history`
---
-
-LOCK TABLES `order_history` WRITE;
-/*!40000 ALTER TABLE `order_history` DISABLE KEYS */;
-/*!40000 ALTER TABLE `order_history` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `order_line`
---
-
-DROP TABLE IF EXISTS `order_line`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `order_line` (
-  `line_id` int NOT NULL,
-  `book_id` int NOT NULL,
-  `order_id` int DEFAULT NULL,
-  `quantity` int DEFAULT NULL,
-  `price_each` int DEFAULT NULL,
-  PRIMARY KEY (`line_id`,`book_id`),
-  KEY `order_id` (`order_id`),
-  KEY `book_id` (`book_id`),
-  CONSTRAINT `order_line_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `cust_order` (`order_id`),
-  CONSTRAINT `order_line_ibfk_2` FOREIGN KEY (`book_id`) REFERENCES `book` (`book_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `order_line`
---
-
-LOCK TABLES `order_line` WRITE;
-/*!40000 ALTER TABLE `order_line` DISABLE KEYS */;
-/*!40000 ALTER TABLE `order_line` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `order_status`
---
-
-DROP TABLE IF EXISTS `order_status`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `order_status` (
-  `status_id` int NOT NULL AUTO_INCREMENT,
-  `status_name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`status_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `order_status`
---
-
-LOCK TABLES `order_status` WRITE;
-/*!40000 ALTER TABLE `order_status` DISABLE KEYS */;
-/*!40000 ALTER TABLE `order_status` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `publisher`
---
-
-DROP TABLE IF EXISTS `publisher`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `publisher` (
-  `publisher_id` int NOT NULL AUTO_INCREMENT,
-  `publisher_name` varchar(100) DEFAULT NULL,
-  `website` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`publisher_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `publisher`
---
-
-LOCK TABLES `publisher` WRITE;
-/*!40000 ALTER TABLE `publisher` DISABLE KEYS */;
-/*!40000 ALTER TABLE `publisher` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `shipping_method`
---
-
-DROP TABLE IF EXISTS `shipping_method`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `shipping_method` (
-  `shipping_method_id` int NOT NULL AUTO_INCREMENT,
-  `method_name` varchar(100) DEFAULT NULL,
-  `cost` int DEFAULT NULL,
-  PRIMARY KEY (`shipping_method_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `shipping_method`
---
-
-LOCK TABLES `shipping_method` WRITE;
-/*!40000 ALTER TABLE `shipping_method` DISABLE KEYS */;
-/*!40000 ALTER TABLE `shipping_method` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2025-04-12 18:08:05
+SELECT 
+    co.order_id,
+    c.first_name,
+    c.last_name,
+    co.order_date,
+    os.status_name AS order_status
+FROM
+    cust_order co
+        JOIN
+    customer c ON co.customer_id = c.customer_id
+        JOIN
+    order_status os ON co.status_id = os.status_id;
